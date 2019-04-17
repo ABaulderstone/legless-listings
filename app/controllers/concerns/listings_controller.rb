a4cb6ffdec7b26fa58c8e8eb926e05e14ca281b1
Alex Baulderstone <alexbaulderstone@gmail.com>
2019-04-17 14:55:25 +1000

    added controllers and methods, views for listings

---
+++ app/controllers/concerns/listings_controller.rb
@@ -0,0 +1,23 @@
+class ListingsController < ApplicationController
+    def create 
+        #create new listing
+    end 
+    def update 
+        #updates current listing
+    end 
+    def index 
+        #shows all listings
+    end 
+    def edit 
+        #shows edit form 
+    end 
+    def destroy 
+        #delete current listing 
+    end 
+    def new 
+        #shows create form 
+    end 
+    def show 
+        #shows current listing
+    end 
+end 
